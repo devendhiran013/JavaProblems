ccf0bc08c2cf6393a8461d2723c37b13fd02be65
Devendhiran <devsavi07@gmail.com>
2024-06-29 17:52:38 +0530

    updated

--- search.java
+++ search.java
@@ -2,26 +2,30 @@ import java.util.Scanner;
 
 public class search {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-       int n=s.nextInt();
-       int x[]=new int[n];
-       int temp=0;
-       for(int i=0;i<n;i++){
-            System.out.println("enter the size ");
-            x[i]=s.nextInt();
-            System.out.println(""+x[i]);
-       }
-       System.out.println("Enter no to search");
-       int a=s.nextInt();
-       for(int j=1;j<n;j++){
-            
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int x[] = new int[n];
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
 
-       }
-       
-        System.out.println("yes"+temp);
-       }
+            x[i] = s.nextInt();
 
+        }
+        System.out.println("Enter no to search");
+        int a = s.nextInt();
+        for (int j = 1; j < n; j++) {
+            if (x[j] == a) {
+                temp = 1;
+            }
 
+        }
+        if (temp == 1) {
 
-
+            System.out.println("yes");
+        }
+        else {
+            System.out.println("No");
+        }
     }
+
+}
