7b339a7640a6768b03b2a2dd23a9a7c35753e3a8
Devendhiran <devsavi07@gmail.com>
2024-07-01 18:28:06 +0530

    added

---
+++ Array/RemoveDuplicate.java
@@ -0,0 +1,30 @@
+package Array;
+
+import java.util.*;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int arr[] = new int[a];
+        int i, j = 0;
+        for (i = 0; i < a; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        for (i = 0; i < a; i++) {
+            int k = 0;
+            for (j = i + 1; j < a; j++) {
+                if (arr[i] == arr[j]) {
+                    k = 1;
+                    
+                }
+
+            }
+
+            if (k != 1) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
