1f96ea30c58c93fc7b871399e12ccd6dc4eb81fd
Devendhiran <devsavi07@gmail.com>
2024-07-02 14:33:48 +0530

    updated

--- Array/Monotonic.java
+++ Array/Monotonic.java
@@ -1,35 +1,32 @@
 package Array;
-
 import java.util.*;
-
 public class Monotonic {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        int arr[] = new int[a];
-
-        for (int i = 0; i < a; i++) {
-            arr[i] = s.nextInt();
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++){
+            arr[i]=s.nextInt();
         }
-
-        boolean isIncreasing = true;
-        boolean isDecreasing = true;
-
-        for (int i = 0; i < a - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                isDecreasing = false;
-            }
-            if (arr[i] > arr[i + 1]) {
-                isIncreasing = false;
+        int k=0,f=0;
+        for(int i=0;i<a;i++){
+           
+             for(int j=i+1;j<a;j++){
+                if(arr[i]>arr[j]){
+                      k=1;
+                }
+                if(arr[i]<arr[j]){
+                    f=1;
+                }
             }
         }
-
-        if (isIncreasing || isDecreasing) {
+       
+        
+        if(k==0||f==0){
             System.out.println("Monotonic");
-        } else {
-            System.out.println("Non-monotonic");
         }
-
-        s.close();
+        else{
+            System.out.println("Non Monotonic");
+        }
     }
 }
