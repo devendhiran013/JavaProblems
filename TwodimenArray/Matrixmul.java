0a1c47449e8ce1d8a52f39a3bcc40e42dba2f994
Devendhiran <devsavi07@gmail.com>
2024-07-03 12:35:45 +0530

    updated

--- TwodimenArray/Matrixmul.java
+++ TwodimenArray/Matrixmul.java
@@ -4,40 +4,39 @@ import java.util.Scanner;
 
 public class Matrixmul {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int a=s.nextInt();
-        int b=s.nextInt();
-        
-        int arr1[][]=new int[a][b];
-        int arr2[][]=new int[a][b];
-        int i,j;
-        for ( i = 0; i < a; i++) {
-            for ( j = 0; j < b; j++) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+
+        int arr1[][] = new int[a][b];
+        int arr2[][] = new int[a][b];
+        int i, j;
+        for (i = 0; i < a; i++) {
+            for (j = 0; j < b; j++) {
                 arr1[i][j] = s.nextInt();
-                
+
             }
         }
-        for ( i = 0; i < a; i++) {
-            for ( j = 0; j < b; j++) {
+        for (i = 0; i < a; i++) {
+            for (j = 0; j < b; j++) {
                 arr2[i][j] = s.nextInt();
-                
+
             }
         }
         System.out.println();
-        
-        int sum[][]=new int[a][b];
 
-        for ( i = 0; i < a; i++) {
+        int sum[][] = new int[a][b];
+
+        for (i = 0; i < a; i++) {
             System.out.println();
-            for ( j = 0; j < b; j++) {
-              for(int k=0;k<a;k++){
-                sum[i][j]+=arr1[i][k]*arr2[k][j];
-                
-               
+            for (j = 0; j < b; j++) {
+                for (int k = 0; k < a; k++) {
+                    sum[i][j] += arr1[i][k] * arr2[k][j];
+
+                }
+                System.out.print(sum[i][j] + " ");
+
             }
-            System.out.print(sum[i][j]+" ");
-            
         }
-        }  
     }
 }
