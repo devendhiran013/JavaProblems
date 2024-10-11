5e12b0f45d7e949d58976d7eb60f3a8b2b3d8ddd
devendhiran013 <devsavi07@gmail.com>
2024-10-11 17:52:33 +0530

    added

--- While/NumtoDigit.java
+++ While/NumtoDigit.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
-        int mul = 0, pv = 1, temp = num;
+        int mul = 0, pv = 1, tem = num;
 
         // Process each digit
-        while(temp != 0) {
-            int d = temp % 10; // Extract the last digit
+        while(tem != 0) {
+            int d = tem % 10; // Extract the last digit
 
             // Replace 0 with 5
             if(d == 0) {
@@ -19,7 +19,7 @@ import java.util.Scanner;
             // Construct the modified number
             mul = d * pv + mul;
             pv *= 10;
-            temp /= 10;
+            tem /= 10;
         }
         System.out.println(mul);
     }
